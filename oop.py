db7c8c9da7893686bd10e0dd380eb7b83b6f1bea
Ndugere <ndungudavid972@gmail.com>
2025-09-16 11:13:13 +0300

    A cleaner more complete version of the code with checker

--- oop.py
+++ oop.py
@@ -1,20 +1,27 @@
 class Student:
     def __init__(self, name, house):
+        if not name:
+            raise ValueError("Missing Name")
         self.name = name
+        if house not in {"Kitengela", "Nairobi", "Kisumu", "Mombasa", "Eldoret"}:
+            raise ValueError("House not in the accepted collection!")
         self.house = house
     
     def __str__(self):
         return f"{self.name} from {self.house}"
+
 def main():
     student = get_student()
-    print(f"{student.name} from {student.house}")
+    print(student)
 
-    
 def get_student():
-    name = input("Enter name: ")
-    house = input("Enter house: ")
-    return Student(name, house)
-
+    while True:
+        name = input("Enter name: ")
+        house = input("Enter house: ")
+        try:
+            return Student(name, house)
+        except ValueError as e:
+            print(f"{e}")
 
 if __name__ == "__main__":
     main()
