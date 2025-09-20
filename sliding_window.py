49e2f8c1da15e775f1eb1d990144e34efb84cdb6
Ndugere <ndungudavid972@gmail.com>
2025-09-20 10:48:13 +0300

    Sliding window to calculate the longest string without repeating

--- sliding_window.py
+++ sliding_window.py
@@ -1,4 +1,22 @@
-def sliding_window(s, target):
+def longest_substring_without_repeating_characters(s):
+    longest, left, right = 0, 0, 0
+    seen_so_far = {}
 
-    
+    while right < len(s):
+        if s[right] in seen_so_far:
+            seen_so_far[s[right]] += 1  
+        else:
+            seen_so_far[s[right]] = 1
+        
+        while seen_so_far[s[right]] > 1:
+            seen_so_far[s[left]] -= 1
+            left += 1 
+
+        longest = max(longest, right + 1 - left)
+
+        right += 1
+
+    return longest
+
+
 
